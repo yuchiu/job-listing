da8eee37a5b42b99ef9a9778a4b88ccc6cff8117
yuchiu <yuchiu2002@hotmail.com>
2018-08-13 20:16:45 -0400

    setup with redux stucture

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "../data/svg/logo.svg";
+import "./index.css";
 
-class App extends Component {
+class Routes extends Component {
   render() {
     return (
       <div className="App">
@@ -18,4 +18,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Routes;
